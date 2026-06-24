26877eff37029234403bf52ab4a51c77ecaa51b6
Sharan-0-dot <sharansc482@gmail.com>
2026-06-24 21:14:02 +0530

    added cors config and dockerized the application

---
+++ src/main/java/com/Sharan/job_search_agent/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.Sharan.job_search_agent.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${app.frontend-url}")
+    private String frontendUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(frontendUrl)
+                .allowedMethods(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "PATCH",
+                        "DELETE",
+                        "OPTIONS"
+                )
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
